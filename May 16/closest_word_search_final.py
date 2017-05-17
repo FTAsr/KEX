9d1f06169aac7185d3f8974c11af89dc6f9e7d1f
Prudhvi Raj Dachapally <prudhviraj693@gmail.com>
2017-05-17 10:19:05 -0400

    updated comments

--- May 16/closest_word_search_final.py
+++ May 16/closest_word_search_final.py
@@ -5,43 +5,33 @@ Created on Tue May 16 16:24:04 2017
 @author: prudh
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 16 13:08:39 2017
+import numpy as np # For fast numerical computing in Python
+np.random.seed(2) # Random number seeder
+              
+import warnings 
+warnings.filterwarnings("ignore") # To ignore gensim warnings on windows systems
 
-@author: prudh
-"""
+import gensim # Library to load and work with pre-trained word2vec models
+import re # Library for regular expressions
+from scipy.spatial.distance import cosine # Distance metric
 
+random_vect = np.random.rand(300) #Random vector if any word is not present in the model
 
-import numpy as np
-np.random.seed(2)
+# Loading pre-trained word2vec model
+diction = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary= True)
 
-import gensim
+#Loading Files
+files = open('testing_2.txt').read().split() # The document to extract keywords
+stop = open('stopwords_en.txt').read().splitlines() # Stopword list
+gt = open('ground_truth_2.txt').read().splitlines() # ground truth file
 
-import os
-import re
-
-from scipy.spatial.distance import cosine
-
-random_vect = np.random.rand(300)
-diction = gensim.models.KeyedVectors.load_word2vec_format('C:\Users\prudh\Quora\GoogleNews-vectors-negative300.bin', binary= True)
-'''
-diction = {}
-
-for f in files:
-    vals = f.split()
-    diction[vals[0]] = np.array(vals[1:]).astype(np.float16)
-
-del files
-'''
-#os.chdir('NLP_2')
-files = open('testing_2.txt').read().split()
-stop = open('stopwords_en.txt').read().splitlines()
-gt = open('ground_truth_2.txt').read().splitlines()
+#Remove all the stop words
 clean_files = []
 for f in files:
     if f.lower() not in stop:
         clean_files.append(f)
+
+# Remove special characters, etc.
 clean_words = []
 for f in clean_files:
     current = re.sub("\.(?!\d)", "", f)
@@ -51,8 +41,10 @@ for f in clean_files:
 
     clean_words.append(current)
 
+# Convert the words to a Numpy array
 words = np.array(clean_words).astype(np.str)
 
+# Create a word2vec matrix
 w2v_matrix = []
 for w in words:
     if w in diction:
@@ -60,33 +52,50 @@ for w in words:
     else:
         w2v_matrix.append(random_vect)
         
+#Converting the values to float16 to make things run faster
 w2v_matrix = np.array(w2v_matrix).astype(np.float16)
 
+'''
+The closest_word_search() takes two parameters:
+    Parameter 1: The word2vec matrix
+    Parameter 2: Maximum Distance Threshold
+    
+The method takes the current word and the proceeding 
+word and calculates the cosine distance between them.
+If this distance is larger than some set "high" threshold,
+then it makes the word unrelated in the context of other words,
+therefore, could be considered as a "filler" word.
+'''
 def closest_word_search(w2v_matrix, threshold):
-    add_list = []  
-    bad_list = []
+    add_list = []  # Initialize list for keywords
+    bad_list = [] # Initialize list for non-keywords 
+    
     for i in range(1, len(w2v_matrix)):
-        current = w2v_matrix[i-1, :]
-        next_ = w2v_matrix[i, :]
-        dist = cosine(current, next_)
-        if dist < threshold:
+    
+        current = w2v_matrix[i-1, :] #current word
+        next_ = w2v_matrix[i, :] # next word
+        
+        dist = cosine(current, next_) #calculate distance
+        
+        if dist < threshold: #Threshold checker
             if words[i] not in add_list:
-                add_list.append(words[i])
+                add_list.append(words[i]) # Add keywords
         else:
-            bad_list.append(words[i])
+            bad_list.append(words[i]) # Add non-keywords
     return add_list, bad_list
     
-concat, bad_list = closest_word_search(w2v_matrix, 0.9)
-print "Predicted Keywords: "
-print concat
-print "Ground Truth: "
+threshold = 0.9
+good_list, bad_list = closest_word_search(w2v_matrix, threshold)
+print "\nPredicted Keywords: "
+print good_list
+print "\nGround Truth: "
 print gt
-print "Matching Keywords: "
-print list(np.intersect1d(concat, gt))
+print "\nMatching Keywords: "
+print list(np.intersect1d(good_list, gt))
 #print '\n\n\n\n'
 #print len(concat)
 #print len(np.intersect1d(concat, gt))
-print "Hit Rate: " +str(1.0 * len(np.intersect1d(concat, gt))/len(gt))
+print "\nHit Rate: " +str(1.0 * len(np.intersect1d(good_list, gt))/len(gt))
 
-print "Non-keyword List: "
+print "\nNon-keyword List: "
 print bad_list
